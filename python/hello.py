5c5b2852df8201641139cd5f12fe0f698c870bcf
RenZe912 <lgxb1324@gmail.com>
2025-09-27 22:17:24 +0800

    using coploit

--- python/hello.py
+++ python/hello.py
@@ -1,8 +1,12 @@
 import numpy as np
 
-x = [1,2,3]
+# 生成 10x10 的随机矩阵
+matrix = np.random.rand(10, 10)
 
-a = np.asarray(x)
+# 求每列的平均值
+col_means = matrix.mean(axis=0)
 
-print(a)
+print("随机矩阵：\n", matrix)
+print("每列的平均值：\n", col_means)
+
 
